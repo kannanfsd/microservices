9c73c8701a438535babc128141b70cf6e4087863
Kannan Rajendran <kannannglabs@gmail.com>
2022-03-13 22:26:24 +0530

    create feign proxy client

--- currency-conversion/src/main/java/com/nglabs/microservices/controller/CurrencyConversionController.java
+++ currency-conversion/src/main/java/com/nglabs/microservices/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.nglabs.microservices.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.nglabs.microservices.model.CurrencyConversion;
+import com.nglabs.microservices.proxy.CurrencyExchangeProxy;
 
 @RestController
 public class CurrencyConversionController {
+	@Autowired
+	private CurrencyExchangeProxy proxy;	
+	
 	@GetMapping("currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(
 			@PathVariable String from,
@@ -26,6 +31,20 @@ public class CurrencyConversionController {
 				CurrencyConversion.class,
 				uriVariables);
 		CurrencyConversion currencyConversion = responseEntity.getBody();
+		return new CurrencyConversion(currencyConversion.getId(), 
+									  from, 
+									  to, 
+									  quantity, 
+									  currencyConversion.getConversionMultiple(), 
+									  quantity.multiply(currencyConversion.getConversionMultiple()), 
+									  currencyConversion.getEnvironment());
+	}
+	@GetMapping("currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversionFeign(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		CurrencyConversion currencyConversion = proxy.retrieveExchangeValue(from, to);
 		return new CurrencyConversion(currencyConversion.getId(), 
 									  from, 
 									  to, 
