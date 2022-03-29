7189a46e2a6230d9336eeb2b5e8c1c4a85b24edb
Kannan Rajendran <kannannglabs@gmail.com>
2022-03-29 16:47:18 +0530

    add zipkin module

--- currency-conversion/src/main/java/com/nglabs/microservices/controller/CurrencyConversionController.java
+++ currency-conversion/src/main/java/com/nglabs/microservices/controller/CurrencyConversionController.java
@@ -27,7 +27,7 @@ public class CurrencyConversionController {
 		uriVariables.put("from", from);
 		uriVariables.put("to", to);
 		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
-				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+				"http://currency-exchange:8000/currency-exchange/from/{from}/to/{to}", 
 				CurrencyConversion.class,
 				uriVariables);
 		CurrencyConversion currencyConversion = responseEntity.getBody();
--- currency-exchange/src/main/java/com/nglabs/microservices/controller/CurrencyExchangeController.java
+++ currency-exchange/src/main/java/com/nglabs/microservices/controller/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.nglabs.microservices.controller;
 
 import java.math.BigDecimal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import com.nglabs.microservices.repository.CurrencyExchangeRepository;
 
 @RestController
 public class CurrencyExchangeController {
+	
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private CurrencyExchangeRepository repository;
 	@Autowired
@@ -23,6 +28,7 @@ public class CurrencyExchangeController {
 			@PathVariable String from,
 			@PathVariable String to) {
 		//CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+		logger.info("retrieveExchangeValue call with {} and {}", from, to);
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		if(currencyExchange == null) {
 			throw new RuntimeException("Unable to find data for "+from+" to "+to);
